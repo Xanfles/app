27e267659a8d330abb7902cfe9594fb517ef552e
Jonathan Alejandro González Pacheco <gonzalezpacheco.jonathan@gmail.com>
2018-01-22 15:47:17 -0300

    fix
    fix

--- www/lib/addUser.js
+++ www/lib/addUser.js
@@ -54,18 +54,22 @@ function sendData(){
 			}
 		},
 		submitHandler: function(form) {
+			alert('entrando');
 			window.plugins.toast.showShortCenter('Holi', function(a){console.log('toast error: ' + a)});
 			compare1 = $('#txtPass').val();
 			compare2 = $('#txtRpass').val();
 			if (compare1 === compare2) {
 				dataString = "nombre="+$('#txtNombre').val()+"&apellido="+$('#txtApellido').val()+"&pass="+$('#txtPass').val()+"&rut="+$('#txtRut').val()+"&fono="+$('#txtFono').val()+"&mail="+$('#txtMail').val()+"&user="+$('#txtRut').val();
 		      	//alert(dataString);
+		      	alert('ajax para enviar');
 			    $.ajax({
+
 			      	url: "http://ws.masmargen.cl/agregar_usuario.php",
 			      	data: dataString,
 			      	type: "post",
 			      	success: function(data){
 			          //alert(data);
+			          alert('ajax enviado ok');
 			          data = jQuery.parseJSON(data);
 			          	if(data == '3'){
 			          		window.plugins.toast.showShortCenter('Usuario Agregado', function(a){console.log('toast success: ' + a)});
