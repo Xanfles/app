4539d433fc67dedda7b9edb260b7d144e649d9ea
Jonathan Alejandro González Pacheco <gonzalezpacheco.jonathan@gmail.com>
2018-01-29 11:35:49 -0300

    fix
    fix

--- platforms/browser/www/lib/addUser.js
+++ platforms/browser/www/lib/addUser.js
@@ -8,66 +8,14 @@ function sendData(){
 		e.preventDefault();
 	}).validate({
 		debug: false,
-		rules: {
-			"txtNombre": {
-				required: true
-			},
-			"txtApellido": {
-				required: true
-			},
-			"txtRut": {
-				required: true
-			},
-			"txtPass": {
-				required: true
-			},
-			"txtRpass": {
-				required: true
-			},
-			"txtFono": {
-				required: true
-			},
-			"txtMail": {
-				required: true
-			}
-		},
-		messages: {
-			"txtNombre": {
-				required: "*"
-			},
-			"txtApellido": {
-				required: "*"
-			},
-			"txtRut": {
-				required: "*"
-			},
-			"txtPass": {
-				required: "*"
-			},
-			"txtRpass": {
-				required: "*"
-			},
-			"txtFono": {
-				required: "*"
-			},
-			"txtMail": {
-				required: "*"
-			}
-		},
 		submitHandler: function(form) {
-			alert("Funcion");
-			//window.plugins.toast.showShortCenter('Holi', function(a){console.log('toast error: ' + a)});
-			compare1 = $('#txtPass').val();
-			compare2 = $('#txtRpass').val();
-			if (compare1 === compare2) {
-				dataString = "nombre="+$('#txtNombre').val()+"&apellido="+$('#txtApellido').val()+"&pass="+$('#txtPass').val()+"&rut="+$('#txtRut').val()+"&fono="+$('#txtFono').val()+"&mail="+$('#txtMail').val()+"&user="+$('#txtRut').val();
-		      	//alert(dataString);
-		      	alert("Enviando");
-		      	$.ajax({
-		      		url: "http://ws.masmargen.cl/agregar_usuario.php",
-		      		data: dataString,
-		      		type: "post",
-		      		success: function(data){
+			dataString = "nombre="+$('#txtNombre').val()+"&apellido="+$('#txtApellido').val()+"&pass="+$('#txtPass').val()+"&rut="+$('#txtRut').val()+"&fono="+$('#txtFono').val()+"&mail="+$('#txtMail').val()+"&user="+$('#txtRut').val();
+			alert("Enviando");
+			$.ajax({
+				url: "http://ws.masmargen.cl/agregar_usuario.php",
+				data: dataString,
+				type: "post",
+				success: function(data){
 			          //alert(data);
 			          data = jQuery.parseJSON(data);
 			          if(data == "3"){
@@ -86,16 +34,12 @@ function sendData(){
 			          	alert("nada");
 			          	window.plugins.toast.showShortCenter('Problemas al agregar usuario', function(a){console.log('toast error: ' + a)});
 			          }
-			      },
-			      error: function(data){
+			    },
+			    error: function(data){
 			      	alert("error");
 			      	toastr.error(data);
-			      }
-			  });
-
-		      }else{
-		      	window.plugins.toast.showShortCenter('Las contraseñas deben ser iguales', function(a){console.log('toast error: ' + a)});
-		      }
-		  }
-		});
+			    }
+			});
+		}
+	});
 }
