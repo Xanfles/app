036c61138ad2e4fcaaf692cdde31fc5360ba4c3d
Xanfles <gonzalezpacheco.jonathan@gmail.com>
2018-04-17 11:46:17 -0300

    fix
    fix

---
+++ platforms/browser/www/lib/login.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+  sendData();
+});
+
+function sendData(){
+  $('#loginform').submit(function(e) {
+    e.preventDefault();
+  }).validate({
+    debug: false,
+    rules: {
+      "txtUsuario": {
+        required: true
+      },
+      "txtClave": {
+        required: true
+      }
+    },
+    messages: {
+      "txtUsuario": {
+        required: "*"
+      },
+      "txtClave": {
+        required: "*"
+      }
+    },
+    submitHandler: function(form) {
+      dataString = "user="+$('#txtUsuario').val()+"&pass="+$('#txtClave').val();
+      var username = $('#txtUsuario').val();
+      //alert(dataString);
+      $.ajax({
+        url: "http://ws.masmargen.cl/login.php",
+        data: dataString,
+        type: "post",
+        success: function(data){
+          //alert(data);
+          data = jQuery.parseJSON(data);
+          if(data == '1'){
+            localStorage.setItem("username", username);
+            window.plugins.toast.showShortCenter('Bienvenido Doctor', function(a){console.log('toast success: ' + a)});
+            setTimeout(function(){
+              window.location.replace("main.html");
+            }, 2000);
+          }else{
+          	window.plugins.toast.showShortCenter('Contraseña o Usuario Invalidos', function(a){console.log('toast error: ' + a)});
+            toastr.error(data[0].message);
+          }
+        },
+        error: function(data){
+          toastr.error(data);
+        }
+      });
+    }
+  });
+}
+
+toastr.options = {
+  "closeButton": false,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-bottom-center",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
